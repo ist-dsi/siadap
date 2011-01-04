a514a01d7e36a5cfb8d117cab8250a95900e6d75
João Antunes <joao.antunes@tagus.ist.utl.pt>
2011-01-04 16:04:35 +0000

    Bug Fix: SIADAP corrected several bugs that existed due to the system's incorrect handling of different years
    Issue: NEW - # 41: Problema com o link de criar e editar, que não usam o ano seleccionado 
    https://fenix-ashes.ist.utl.pt/trac/siadap/ticket/41

--- src/module/siadap/presentationTier/renderers/providers/SiadapYearsFromExistingSiadapConfigurations.java
+++ src/module/siadap/presentationTier/renderers/providers/SiadapYearsFromExistingSiadapConfigurations.java
@@ -5,48 +5,63 @@ package module.siadap.presentationTier.renderers.providers;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.List;
 
 import module.siadap.domain.SiadapRootModule;
 import module.siadap.domain.SiadapYearConfiguration;
-import module.siadap.presentationTier.actions.SiadapManagement;
 import pt.ist.fenixWebFramework.renderers.DataProvider;
 import pt.ist.fenixWebFramework.renderers.components.converters.Converter;
 
 /**
  * @author joantune (joao.antunes@tagus.ist.utl.pt)
- *
+ * 
  */
 public class SiadapYearsFromExistingSiadapConfigurations implements DataProvider {
 
-	/* (non-Javadoc)
-	 * @see pt.ist.fenixWebFramework.renderers.DataProvider#provide(java.lang.Object, java.lang.Object)
-	 */
-	@Override
-	public Object provide(Object source, Object currentValue) {
-		ArrayList<Integer> years = new ArrayList<Integer>();
-		for (SiadapYearConfiguration siadapYearConfiguration : SiadapRootModule.getInstance().getYearConfigurations()) {
-			years.add(new Integer(siadapYearConfiguration.getYear()));
-		}
-		Collections.sort(years);
-		return years;
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * pt.ist.fenixWebFramework.renderers.DataProvider#provide(java.lang.Object,
+     * java.lang.Object)
+     */
+    @Override
+    public Object provide(Object source, Object currentValue) {
+	return getYearsWithExistingConfigs();
+    }
 
-	/* (non-Javadoc)
-	 * @see pt.ist.fenixWebFramework.renderers.DataProvider#getConverter()
-	 */
-	@Override
-	public Converter getConverter() {
-		return null;
-//		return new Converter() {
-//			
-//			@Override
-//			public Object convert(Class type, Object value) {
-//				if (value != null)
-//					return ((SiadapYearConfiguration) value).getYear();
-//				return null;
-//			}
-//		};
+    /**
+     * Method wrapper which is used by other interface packages other than the
+     * renders
+     * 
+     * @return an {@link ArrayList} of Integers with the years that have a
+     *         configuration associated
+     */
+    static public ArrayList<Integer> getYearsWithExistingConfigs() {
+	ArrayList<Integer> years = new ArrayList<Integer>();
+	for (SiadapYearConfiguration siadapYearConfiguration : SiadapRootModule.getInstance().getYearConfigurations()) {
+	    years.add(new Integer(siadapYearConfiguration.getYear()));
 	}
+	Collections.sort(years);
+	return years;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see pt.ist.fenixWebFramework.renderers.DataProvider#getConverter()
+     */
+    @Override
+    public Converter getConverter() {
+	return null;
+	// return new Converter() {
+	//
+	// @Override
+	// public Object convert(Class type, Object value) {
+	// if (value != null)
+	// return ((SiadapYearConfiguration) value).getYear();
+	// return null;
+	// }
+	// };
+    }
 
 }
