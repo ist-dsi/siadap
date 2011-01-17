fedc36332c4055df8daafa58f00e6e9ab11e3817
João Antunes <joao.antunes@tagus.ist.utl.pt>
2011-01-17 12:33:40 +0000

    Minor Change: SIADAP: When creating the competences for an evaluated, the PersonEvaluatedOnlyByCompetences now defaults to false. Issue: CLOSED - # 70: Por omissão estar seleccionado que o avaliado não é avaliado apenas por competências https://fenix-ashes.ist.utl.pt/trac/siadap/ticket/70

--- src/module/siadap/activities/CreateOrEditCompetenceEvaluationActivityInformation.java
+++ src/module/siadap/activities/CreateOrEditCompetenceEvaluationActivityInformation.java
@@ -12,92 +12,89 @@ import module.workflow.activities.ActivityInformation;
 import module.workflow.activities.WorkflowActivity;
 import module.workflow.domain.WorkflowProcess;
 
-public class CreateOrEditCompetenceEvaluationActivityInformation extends
-		ActivityInformation<SiadapProcess> implements ContainsCompetenceType {
+public class CreateOrEditCompetenceEvaluationActivityInformation extends ActivityInformation<SiadapProcess> implements
+	ContainsCompetenceType {
 
-	private Siadap siadap;
-	private CompetenceType competenceType;
-	private List<Competence> competences;
-	// Variable used to make sure that the JSP is displayed to the user upon
-	// creation/edition of the competences
-	private boolean inputDisplayed = false;
+    private Siadap siadap;
+    private CompetenceType competenceType;
+    private List<Competence> competences;
+    // Variable used to make sure that the JSP is displayed to the user upon
+    // creation/edition of the competences
+    private boolean inputDisplayed = false;
 
-	private Boolean evaluatedOnlyByCompetences;
+    private Boolean evaluatedOnlyByCompetences;
 
-	public CreateOrEditCompetenceEvaluationActivityInformation(
-			SiadapProcess process,
-			WorkflowActivity<? extends WorkflowProcess, ? extends ActivityInformation> activity) {
-		super(process, activity);
-		setCompetences(new ArrayList<Competence>(getSiadap().getCompetences()));
-//		setCompetences(process.getSiadap().getCompetences());
-		setCompetenceType(process.getSiadap().getCompetenceType());
-		setEvaluatedOnlyByCompetences(process.getSiadap()
-				.getEvaluatedOnlyByCompetences());
+    public CreateOrEditCompetenceEvaluationActivityInformation(SiadapProcess process,
+	    WorkflowActivity<? extends WorkflowProcess, ? extends ActivityInformation> activity) {
+	super(process, activity);
+	setCompetences(new ArrayList<Competence>(getSiadap().getCompetences()));
+	// setCompetences(process.getSiadap().getCompetences());
+	setCompetenceType(process.getSiadap().getCompetenceType());
+	if (process.getSiadap().getEvaluatedOnlyByCompetences() == null) {
+	    setEvaluatedOnlyByCompetences(Boolean.FALSE);
+	} else {
+	    setEvaluatedOnlyByCompetences(process.getSiadap().getEvaluatedOnlyByCompetences());
 	}
+    }
 
-	@Override
-	public void setProcess(SiadapProcess process) {
-		super.setProcess(process);
-		setSiadap(process.getSiadap());
-	}
+    @Override
+    public void setProcess(SiadapProcess process) {
+	super.setProcess(process);
+	setSiadap(process.getSiadap());
+    }
 
-	public Siadap getSiadap() {
-		return siadap;
-	}
+    public Siadap getSiadap() {
+	return siadap;
+    }
 
-	public void setSiadap(Siadap siadap) {
-		this.siadap = siadap;
-	}
-	
+    public void setSiadap(Siadap siadap) {
+	this.siadap = siadap;
+    }
 
-	public void setCompetenceType(CompetenceType competenceType) {
-		//clean the other fields if this is changed
-		if (this.competenceType != null && this.competenceType != competenceType)
-		{
-//			setCompetences(new ArrayList<Competence>());			
-			getCompetences().clear();
-			setInputDisplayed(false);
-		}
-		this.competenceType = competenceType;
+    public void setCompetenceType(CompetenceType competenceType) {
+	// clean the other fields if this is changed
+	if (this.competenceType != null && this.competenceType != competenceType) {
+	    // setCompetences(new ArrayList<Competence>());
+	    getCompetences().clear();
+	    setInputDisplayed(false);
 	}
+	this.competenceType = competenceType;
+    }
 
-	public CompetenceType getCompetenceType() {
-		return competenceType;
-	}
+    @Override
+    public CompetenceType getCompetenceType() {
+	return competenceType;
+    }
 
-	public void setCompetences(List<Competence> competences) {
-		this.competences = competences;
-	}
+    public void setCompetences(List<Competence> competences) {
+	this.competences = competences;
+    }
 
-	public List<Competence> getCompetences() {
-		return competences;
-	}
+    public List<Competence> getCompetences() {
+	return competences;
+    }
 
-	@Override
-	public boolean hasAllneededInfo() {
-		return isForwardedFromInput()
-				&& getInputDisplayed()
-				&& getSiadap() != null
-				&& getCompetences() != null
-				&& getCompetenceType() != null
-				&& (evaluatedOnlyByCompetences != null);
-	}
+    @Override
+    public boolean hasAllneededInfo() {
+	return isForwardedFromInput() && getInputDisplayed() && getSiadap() != null && getCompetences() != null
+		&& getCompetenceType() != null && (evaluatedOnlyByCompetences != null);
+    }
 
-	public void setInputDisplayed(boolean inputDisplayed) {
-		this.inputDisplayed = inputDisplayed;
-	}
+    public void setInputDisplayed(boolean inputDisplayed) {
+	this.inputDisplayed = inputDisplayed;
+    }
 
-	public boolean getInputDisplayed() {
-		return inputDisplayed;
-	}
+    public boolean getInputDisplayed() {
+	return inputDisplayed;
+    }
 
-	public void setEvaluatedOnlyByCompetences(Boolean evaluatedOnlyByCompetences) {
-		this.evaluatedOnlyByCompetences = evaluatedOnlyByCompetences;
-	}
+    public void setEvaluatedOnlyByCompetences(Boolean evaluatedOnlyByCompetences) {
+	this.evaluatedOnlyByCompetences = evaluatedOnlyByCompetences;
+    }
 
-	@Override
-	public Boolean getEvaluatedOnlyByCompetences() {
-		return evaluatedOnlyByCompetences;
-	}
+    @Override
+    public Boolean getEvaluatedOnlyByCompetences() {
+	return evaluatedOnlyByCompetences;
+    }
 
 }
