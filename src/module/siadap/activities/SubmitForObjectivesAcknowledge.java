e2027604b7a2994ae8858c4b6dd98df9f4ba355e
João Antunes <joao.antunes@tagus.ist.utl.pt>
2011-02-25 20:32:37 +0000

    Bug Fix: SIADAP: Corrected the send e-mail method when submitting the Objectives and Competences for acknowledgement of the evaluated Issue: NEW - # 101: Corrigir os problemas relacionados com as pessoas/e-mails inexistentes https://fenix-ashes.ist.utl.pt/trac/siadap/ticket/101

--- src/module/siadap/activities/SubmitForObjectivesAcknowledge.java
+++ src/module/siadap/activities/SubmitForObjectivesAcknowledge.java
@@ -2,7 +2,10 @@ package module.siadap.activities;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.ResourceBundle;
 
+import module.organization.domain.Person;
+import module.organizationIst.domain.listner.LoginListner;
 import module.siadap.domain.Siadap;
 import module.siadap.domain.SiadapProcess;
 import module.workflow.activities.ActivityInformation;
@@ -14,6 +17,7 @@ import org.joda.time.LocalDate;
 
 import pt.ist.emailNotifier.domain.Email;
 import pt.ist.fenixframework.plugins.remote.domain.exception.RemoteException;
+import pt.utl.ist.fenix.tools.util.i18n.Language;
 
 public class SubmitForObjectivesAcknowledge extends WorkflowActivity<SiadapProcess, ActivityInformation<SiadapProcess>> {
 
@@ -25,6 +29,33 @@ public class SubmitForObjectivesAcknowledge extends WorkflowActivity<SiadapProce
 		&& siadap.getRequestedAcknowledgeDate() == null && siadap.getObjectivesAndCompetencesSealedDate() != null;
     }
 
+    private void checkEmailExistenceImportAndWarnOnError(Person person) {
+	//if we have no info about the person, let's import it
+	if (person.getRemotePerson() == null || person.getRemotePerson().getEmailForSendingEmails() == null) {
+	    LoginListner.importUserInformation(person.getUser().getUsername());
+	}
+	//if that didn't solved it, let's warn the admin by e-mail
+	if (person.getRemotePerson() == null || person.getRemotePerson().getEmailForSendingEmails() == null) {
+	    StringBuilder message = new StringBuilder("Error, could not import e-mail/info for person " + person.getName() + "\n");
+	    if (person.getUser() != null && person.getUser().getUsername() != null) {
+		message.append("the username is: " + person.getUser().getUsername() + "\n");
+
+	    }
+	    message.append("Please take appropriate actions\n");
+	    notifyAdmin("[Bennu/Myorg] - Error retrieving remote information from fenix for a user", message.toString());
+
+	}
+
+    }
+
+    //TODO change this so that the e-mail isn't hardcoded and there is a batch sent not for each error an e-mail
+    private void notifyAdmin(String subject, String message) {
+	ArrayList<String> toAddress = new ArrayList<String>();
+	toAddress.add("joao.antunes@tagus.ist.utl.pt");
+	new Email("Aplicação SIADAP", "noreply@ist.utl.pt", new String[] {}, toAddress, Collections.EMPTY_LIST,
+		Collections.EMPTY_LIST, subject, message);
+    }
+
     @Override
     protected void process(ActivityInformation<SiadapProcess> activityInformation) {
 	if (!activityInformation.getProcess().getSiadap().hasAllEvaluationItemsValid())
@@ -33,11 +64,33 @@ public class SubmitForObjectivesAcknowledge extends WorkflowActivity<SiadapProce
 	activityInformation.getProcess().getSiadap().setRequestedAcknowledgeDate(new LocalDate());
 	Siadap siadap = activityInformation.getProcess().getSiadap();
 	ArrayList<String> toAddress = new ArrayList<String>();
+	ArrayList<String> ccAddress = new ArrayList<String>();
+	Person evaluatorPerson = null;
+	String emailEvaluator = null;
 	try {
-	    final String email = activityInformation.getProcess().getSiadap().getEvaluated().getRemotePerson()
-		    .getEmailForSendingEmails();
-	    if (email != null) {
-		toAddress.add(email);
+	    evaluatorPerson = activityInformation.getProcess().getSiadap().getEvaluator().getPerson();
+	    checkEmailExistenceImportAndWarnOnError(evaluatorPerson);
+	    emailEvaluator = evaluatorPerson.getRemotePerson().getEmailForSendingEmails();
+
+	} catch (RemoteException ex) {
+	    if (evaluatorPerson != null) {
+		System.out.println("Could not get e-mail for evaluator " + evaluatorPerson.getName());
+	    } else {
+		System.out.println("Could not get e-mail for evaluator which has no Person object associated!");
+	    }
+	}
+
+	try {
+	    final Person evaluatedPerson = activityInformation.getProcess().getSiadap().getEvaluated();
+	    checkEmailExistenceImportAndWarnOnError(evaluatedPerson);
+	    String emailEvaluated = evaluatedPerson.getRemotePerson().getEmailForSendingEmails();
+
+	    if (emailEvaluated != null) {
+		toAddress.add(emailEvaluated);
+		if (emailEvaluator != null) {
+		    ccAddress.add(emailEvaluator);
+		}
+
 		StringBuilder body = new StringBuilder(
 			"Encontram-se disponiveis em https://dot.ist.utl.pt os objectivos e competências relativos ao ano de "
 				+ siadap.getYear() + ".\n");
@@ -45,15 +98,17 @@ public class SubmitForObjectivesAcknowledge extends WorkflowActivity<SiadapProce
 		body.append("\n\n---\n");
 		body.append("Esta mensagem foi enviada por meio das Aplicações Centrais do IST.\n");
 
-		new Email("Aplicação SIADAP do IST", "noreply@ist.utl.pt", new String[] {}, toAddress, Collections.EMPTY_LIST,
+		new Email("Aplicação SIADAP do IST", "noreply@ist.utl.pt", new String[] {}, toAddress, ccAddress,
 			Collections.EMPTY_LIST, "SIADAP - Tomada de conhecimento de objectivos e competências", body.toString());
 	    }
-	} catch (final RemoteException ex) {
+	} catch (RemoteException ex) {
 	    System.out.println("Unable to lookup email address for: "
-		    + activityInformation.getProcess().getSiadap().getEvaluated().getUser().getUsername()
- + " Error: "
+		    + activityInformation.getProcess().getSiadap().getEvaluated().getUser().getUsername() + " Error: "
 		    + ex.getMessage());
+	    throw new DomainException("error.message.could.not.send.email.now", ResourceBundle.getBundle(
+		    "resources/SiadapResources", Language.getLocale()));
 	}
+
     }
 
     @Override
