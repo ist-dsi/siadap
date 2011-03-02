0395b90e007093e504b928bfe0b90cf77b9c4e29
João Antunes <joao.antunes@tagus.ist.utl.pt>
2011-03-02 04:53:57 +0000

    Bug Fix: SIADAP: added missing files that should have been commited with the previous commit

---
+++ src/module/siadap/domain/groups/SiadapScheduleEditorsGroup.java
@@ -0,0 +1,59 @@
+package module.siadap.domain.groups;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import module.organization.domain.Person;
+import module.siadap.domain.SiadapRootModule;
+import module.siadap.domain.SiadapYearConfiguration;
+import myorg.domain.User;
+import myorg.util.BundleUtil;
+
+import org.joda.time.LocalDate;
+
+public class SiadapScheduleEditorsGroup extends SiadapScheduleEditorsGroup_Base {
+    
+    public SiadapScheduleEditorsGroup() {
+        super();
+    }
+
+    public static SiadapScheduleEditorsGroup getInstance() {
+	return SiadapRootModule.getInstance().getSiadapScheduleEditorsGroup();
+    }
+
+    @Override
+    public boolean isMember(User user) {
+	//get the current year and use it to assert
+	return isMember(user, new LocalDate().getYear());
+    }
+
+    public boolean isMember(User user, Integer year) {
+	SiadapYearConfiguration configuration = SiadapYearConfiguration.getSiadapYearConfiguration(year);
+	return configuration.isPersonMemberOfScheduleExtenders(user.getPerson());
+    }
+
+    @Override
+    public String getName() {
+	return BundleUtil.getStringFromResourceBundle("resources/SiadapResources","siadap.group.name.SiadapScheduleEditorsGroup");
+    }
+
+    @Override
+    public Set<User> getMembers() {
+	return getMembers(new LocalDate().getYear());
+    }
+    
+    public Set<User> getMembers(Integer year) {
+	SiadapYearConfiguration configuration = SiadapYearConfiguration.getSiadapYearConfiguration(year);
+	Set<Person> groupPersons = configuration.getScheduleExtendersSet();
+
+	Set<User> setToReturn = new HashSet<User>();
+
+	for (Person person : groupPersons) {
+	    setToReturn.add(person.getUser());
+	}
+
+	return setToReturn;
+
+    }
+
+}
