781f5fee0d0d6b72a91d855e26d0cecf85521d2d
João Antunes <joao.antunes@tagus.ist.utl.pt>
2011-03-02 04:59:57 +0000

    Bug Fix: added a missing file to the SVN

---
+++ src/module/siadap/domain/SiadapProcessSchedulesEnum.java
@@ -0,0 +1,26 @@
+package module.siadap.domain;
+
+import module.contacts.domain.PartyContactType;
+import myorg.util.BundleUtil;
+import pt.ist.fenixWebFramework.rendererExtensions.util.IPresentableEnum;
+
+public enum SiadapProcessSchedulesEnum implements IPresentableEnum {
+    OBJECTIVES_SPECIFICATION_BEGIN_DATE, OBJECTIVES_SPECIFICATION_END_DATE,
+
+    AUTOEVALUATION_BEGIN_DATE, AUTOEVALUATION_END_DATE,
+
+    EVALUATION_BEGIN_DATE, EVALUATION_END_DATE;
+
+
+    private SiadapProcessSchedulesEnum() {
+    }
+
+
+    @Override
+    public String getLocalizedName() {
+	return BundleUtil.getStringFromResourceBundle("resources/SiadapResources", PartyContactType.class.getSimpleName() + "."
+		+ name());
+
+    }
+
+}
