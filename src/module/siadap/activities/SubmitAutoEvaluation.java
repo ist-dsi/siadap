df42a7408211884ef184c0c8ce334cfe6bf754a6
João Antunes <joao.antunes@tagus.ist.utl.pt>
2011-11-14 15:28:57 +0000

    Minor Change: Corrected the previous SVN commit (it was missing one file to it)

---
+++ src/module/siadap/activities/SubmitAutoEvaluation.java
@@ -0,0 +1,50 @@
+/**
+ * 
+ */
+package module.siadap.activities;
+
+import module.siadap.domain.Siadap;
+import module.siadap.domain.SiadapProcess;
+import module.workflow.activities.ActivityInformation;
+import module.workflow.activities.WorkflowActivity;
+import myorg.domain.User;
+
+/**
+ * @author João Antunes (joao.antunes@tagus.ist.utl.pt) - 11 de Nov de 2011
+ * 
+ *         This activity allows you to have intermediate states when doing the
+ *         auto evaluation so that the evaluatee can save intermediate states of
+ *         his auto evaluation and iterate at will before making it public
+ * 
+ * 
+ */
+public class SubmitAutoEvaluation extends WorkflowActivity<SiadapProcess, ActivityInformation<SiadapProcess>> {
+
+    @Override
+    public boolean isActive(SiadapProcess process, User user) {
+	Siadap siadap = process.getSiadap();
+	return siadap.getEvaluated().getUser().equals(user) && !siadap.isAutoEvaliationDone()
+		&& new AutoEvaluation().isActive(process, user);
+    }
+
+    @Override
+    protected void process(ActivityInformation<SiadapProcess> activityInformation) {
+	//kinda do nothing is ok! :)
+    }
+
+    @Override
+    public boolean isConfirmationNeeded(SiadapProcess process) {
+	return true;
+    }
+
+    @Override
+    public String getUsedBundle() {
+	return "resources/SiadapResources";
+    }
+
+    protected static void revertProcess(ActivityInformation<SiadapProcess> activityInformation) {
+	//TODO also test it! SIADAP-134
+    }
+
+
+}
