df2ed9f1d751c60512e578e98f2388f17fbd7fad
João Antunes <joao.antunes@tagus.ist.utl.pt>
2012-07-11 13:52:24 +0000

    Bug Fix: small NPE solved

--- src/module/siadap/domain/wrappers/PersonSiadapWrapper.java
+++ src/module/siadap/domain/wrappers/PersonSiadapWrapper.java
@@ -923,7 +923,7 @@ public class PersonSiadapWrapper extends PartyWrapper implements Serializable {
 	//if he has already submitted the eval objectives and competences for the evaluated person, we shouldn't do this, 
 	//unless the unit stays the same (therefore he should be changing the quotas), thus we must check if the quotas can be changed i.e. he is waiting harmonization
 
-	if (getWorkingUnit().getUnit().equals(unit)) {
+	if (getWorkingUnit() != null && getWorkingUnit().getUnit().equals(unit)) {
 	    //let's check the state
 	    int currentStateOrdinal = getSiadap().getState().ordinal();
 	    //if we already reached the waiting harmonization state *and the period for it already begun* we need to send an exception
