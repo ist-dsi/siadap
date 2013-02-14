af68b6978a025847f0b6c44e6eb63137decc780c
João Antunes <joao.antunes@tagus.ist.utl.pt>
2013-02-14 15:00:22 +0000

    Minor Change: SIADAP - Harmonization setLockHarmonizationOnQuota and setLockHarmonizationOnQuotaOutsideOfQuotaUniverses set by default to TRUE

--- src/main/java/module/siadap/domain/SiadapYearConfiguration.java
+++ src/main/java/module/siadap/domain/SiadapYearConfiguration.java
@@ -10,7 +10,7 @@
  *
  *   The SIADAP Module is free software: you can
  *   redistribute it and/or modify it under the terms of the GNU Lesser General
- *   Public License as published by the Free Software Foundation, either version 
+ *   Public License as published by the Free Software Foundation, either version
  *   3 of the License, or (at your option) any later version.
  *
  *   The SIADAP Module is distributed in the hope that it will be useful,
@@ -191,7 +191,8 @@ public class SiadapYearConfiguration extends SiadapYearConfiguration_Base {
         setReviewCommissionWaitingPeriod(reviewCommissionWaitingPeriod);
 
         setSiadapRootModule(SiadapRootModule.getInstance());
-        setLockHarmonizationOnQuota(Boolean.FALSE);
+        setLockHarmonizationOnQuota(Boolean.TRUE);
+        setLockHarmonizationOnQuotaOutsideOfQuotaUniverses(Boolean.TRUE);
         setClosedValidation(Boolean.FALSE);
     }
 
